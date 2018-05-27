5275183a4c9222c21269da7e2f9244aa75db511c
Ngoc Minh <tranngocminhcdn@gmail.com>
2018-05-27 13:55:45 +0700

    Update decisionTree.py

--- DecisionTree/decisionTree.py
+++ DecisionTree/decisionTree.py
@@ -18,51 +18,51 @@ def calcEntropy(dataSet):
   keyCounts = {}
   # for each instance or row 
   for instance in dataSet:
-		  # return value in the final column
-		  currentKey = instance[-1]
-		  # If a key was not encountered previously, one is created
-		  if currentKey not in keyCounts.keys(): keyCounts[currentKey] = 0
-		  # For each key, keep track of how many times this key occurs
-		  keyCounts[currentKey] += 1
+	# return value in the final column
+	currentKey = instance[-1]
+	# If a key was not encountered previously, one is created
+	if currentKey not in keyCounts.keys(): keyCounts[currentKey] = 0
+	# For each key, keep track of how many times this key occurs
+	keyCounts[currentKey] += 1
   # our entropy
   entropy = 0.0
   # for each key
   for key in keyCounts:
-		  # calculate the probability of this key
-		  prob = float(keyCounts[key])/numIntances 
-		  # calculate entropy
-		  entropy -= prob * log(prob,2)
-	return entropy
+	# calculate the probability of this key
+	prob = float(keyCounts[key])/numIntances 
+	# calculate entropy
+	entropy -= prob * log(prob,2)
+  return entropy
 
 # split the dataset based on a specific feature and its value
 def splitDataSet(dataSet, feature, value):
   subDataSet = []
   for row in dataSet:
-		if row[feature] == value:
-			reducedRow = row[:feature]
-			reducedRow.extend(row[feature+1:])
-			subDataSet.append(reduceRow)
-	return subDataSet
+	if row[feature] == value:
+		reducedRow = row[:feature]
+		reducedRow.extend(row[feature+1:])
+		subDataSet.append(reduceRow)
+  return subDataSet
  
  def infoGain(dataSet, feature):
-	  # calculate entropy of dataset
-	  baseEntropy = calcEntropy(dataSet)
-	  # entropy of subdataset
-	  newEntropy = 0.0
-	  featList = [example[feature] for example in dataSet]
-	  # Get list of unique values, ex: [0,0,1,1,1] -> {0,1}
-	  uniqueVals = set(featList)
-    # iterate over all the unique values of this feature and split the data # for each feature
-    for value in uniqueVals:
+   # calculate entropy of dataset
+   baseEntropy = calcEntropy(dataSet)
+   # entropy of subdataset
+   newEntropy = 0.0
+   featList = [example[feature] for example in dataSet]
+   # Get list of unique values, ex: [0,0,1,1,1] -> {0,1}
+   uniqueVals = set(featList)
+   # iterate over all the unique values of this feature and split the data # for each feature
+   for value in uniqueVals:
 	    # split dataset based on feature and value
-		  subDataSet = splitDataSet(dataSet, feature, value)
-		  # The new entropy is calculated and summed up for all the unique
-      # values of that feature
-		  prob = len(subDataSet)/float(len(dataSet))
-		  newEntropy += prob * calcEntropy(subDataSet)
-	  # the information gain of feature is the reduction in entropy
-	  gain = baseEntropy - newEntropy
-	  return infoGain
+	    subDataSet = splitDataSet(dataSet, feature, value)
+	    # The new entropy is calculated and summed up for all the unique
+            # values of that feature
+	    prob = len(subDataSet)/float(len(dataSet))
+	    newEntropy += prob * calcEntropy(subDataSet)
+	    # the information gain of feature is the reduction in entropy
+    gain = baseEntropy - newEntropy
+    return infoGain
 
 def chooseBestFeature(dataSet):
 	numFeatures = len(dataSet[0]) - 1
